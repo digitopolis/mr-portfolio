8bfab135056fb26af760cb818cc4efb3c9d9c715
Matt Readout <matt.readout@gmail.com>
2019-06-15 18:45:45 -0400

    define typography settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,20 @@
  */
 
 module.exports = {
-  /* Your site config here */
+	plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto Mono`,
+            variants: [`400`, `700`]
+          },
+          {
+            family: `Roboto`
+          },
+        ],
+      },
+    }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+	<div>
+		<h1>This is my H1 headline</h1>
+		<p>this is just basic text! how does roboto look?</p>
+	</div>
+)
