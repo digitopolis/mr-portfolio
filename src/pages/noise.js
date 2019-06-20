f54626899b5e19b3c30d09d117a1c1b3bc23b1c8
Matt Readout <matt.readout@gmail.com>
2019-06-20 15:06:30 -0400

    connect audiocontext

--- src/pages/noise.js
+++ src/pages/noise.js
@@ -2,21 +2,38 @@ import React from 'react'
 import Layout from '../components/layout'
 import styled from 'styled-components'
 
+
+
+let audioContext
+let osc
+let gain
+
 class Noise extends React.Component {
 
 	state = {
 		on: false,
-		wave: ''
+		wave: '',
+		started: false
 	}
 
 	componentDidMount() {
 		console.log('mounted');
+		// osc.connect(gain)
+		//
+		// osc.type = 'sine'
+		// osc.frequency.value = 120
+		// gain.gain.value = 0.05
 	}
 
 	toggleOnOff = () => {
 		const on = !this.state.on
 		console.log(on);
 		this.setState({ on })
+		if (on) {
+			gain.connect(audioContext.destination)
+		} else {
+			gain.disconnect(audioContext.destination)
+		}
 	}
 
 	handleWaveSelect = (e) => {
@@ -25,24 +42,46 @@ class Noise extends React.Component {
 		this.setState({ wave })
 	}
 
+	handleMouseMove = (event) => {
+    osc.frequency.value = Math.abs(475 - event.clientX) / 100 * 1300
+    gain.gain.value = (393 - event.clientY) / 100 * .5
+  }
+
+	startOscillator = () => {
+		audioContext = new (window.AudioContext || window.webkitAudioContext)()
+		osc = audioContext.createOscillator()
+		gain = audioContext.createGain()
+
+		osc.connect(gain)
+
+		osc.type = 'sine'
+		osc.frequency.value = 120
+		gain.gain.value = 0.05
+
+		osc.start()
+	}
+
 	render () {
 		return (
 			<Layout>
 				<div>
-					<canvas style={{width: "60%", height: "40%", border:'1px solid #000000' }} >
+					<canvas
+						style={{width: "60%", height: "40%", border:'1px solid #000000' }}
+						onMouseMove={this.handleMouseMove} >
 					</canvas>
 				</div>
 				<div id="synth-control">
+					<Button onClick={this.startOscillator}>Start</Button>
 					<Button onClick={this.toggleOnOff} >On/Off</Button>
 					<span id="wave-span">
 						<Button
-							value='Sine'
+							value='sine'
 							onClick={this.handleWaveSelect}>Sine</Button>
 						<Button
-							value='Square'
+							value='square'
 							onClick={this.handleWaveSelect}>Square</Button>
 						<Button
-							value='Saw'
+							value='saw'
 							onClick={this.handleWaveSelect}>Saw</Button>
 					</span>
 				</div>
