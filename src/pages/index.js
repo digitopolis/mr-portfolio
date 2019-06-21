cfd0f626e611beab7e319d1b8ada9fb900e2b188
Matt Readout <matt.readout@gmail.com>
2019-06-21 18:10:36 -0400

    add welcome paragraph and wave animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 export default () => (
 	<Layout>
 		<div className='content'>
-			<h1>This is my H1 headline</h1>
-			<p>this is just basic text! how does roboto look?</p>
+			<h1>Hi, I'm Matt. <span aria-label='waving hand' role='img' class="wave">👋</span></h1>
+			<p>I'm a web developer based in Brooklyn. In my previous l̶i̶f̶e̶ career, I've worked in production music for film and tv, managed the inventory of a rare book store, and run a small letterpress print shop. I love things both low- and high-tech, and I especially like collaborating with people to build (or break, and then fix) cool stuff! I specialize in Ruby on Rails, JavaScript, and React.js but am always excited to learn and play around with new tech.</p>
 		</div>
 	</Layout>
 )
