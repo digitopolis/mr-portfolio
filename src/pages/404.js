dc716d64d7d8cbacf58c1d91383c80b90675f221
Matt Readout <matt.readout@gmail.com>
2019-06-22 13:48:31 -0400

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+	<Layout>
+		<h1>Uh oh. That page doesn't exist! <span aria-label='grimacing face' role='img'>😬</span></h1>
+	</Layout>
+)
