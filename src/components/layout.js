50a77dd8c57f868070713a639bf937c6d0fd8ea9
Matt Readout <matt.readout@gmail.com>
2019-06-22 13:35:13 -0400

    add favicon and seo stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Helmet } from 'react-helmet'
 
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -9,6 +10,14 @@ const ListLink = props => (
 
 export default ({ children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
+		<Helmet>
+			<title>Matt Readout - Web Developer</title>
+      <meta charSet="utf-8" />
+			<meta name="description" content="Matt Readout Web Dev Portfolio">
+		  <meta name="keywords" content="Web Developer, Software Engineer, JavaScript, React, Ruby, Rails">
+		  <meta name="author" content="Matt Readout">
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </Helmet>
 		<header style={{ marginBottom: `3rem` }}>
 			<Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
 				<h3 style={{ display: `inline` }}>Matt Readout</h3>
