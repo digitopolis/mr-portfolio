670be0394bd2380b1098fe9a178f5e61918be0ca
Matt Readout <matt.readout@gmail.com>
2019-06-30 11:26:00 -0400

    build breakout stub

---
+++ src/pages/breakout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Breakout extends React.Component {
+	render () {
+		return (
+			<Layout>
+				<div className='content'>
+					<h1>Break some blocks</h1>
+				</div>
+			</Layout>
+		)
+	}
+}
+
+export default Breakout;
