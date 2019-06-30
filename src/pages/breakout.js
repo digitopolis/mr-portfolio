8cff13af7c240fe558be3a64b829f61ba5c9f24a
Matt Readout <matt.readout@gmail.com>
2019-06-30 12:09:27 -0400

    add styled button

--- src/pages/breakout.js
+++ src/pages/breakout.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import Layout from '../components/layout'
+import styled	 from 'styled-components'
 
 let canvas
 let ctx
+let x
+let y
+let dx
+let dy
 
 class Breakout extends React.Component {
 
 	componentDidMount() {
 		canvas = document.getElementById('gameCanvas')
 		ctx = canvas.getContext('2d')
+	}
+
+	drawBall = () => {
+		ctx.beginPath()
+		ctx.arc(x, y, 10, 0, Math.PI*2)
+		ctx.fillStyle = 'black'
+		ctx.fill()
+		ctx.closePath()
+	}
+
+	draw = () => {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+    this.drawBall()
+    x += dx
+    y += dy
 	}
 
 	render () {
@@ -22,6 +42,9 @@ class Breakout extends React.Component {
 							id='gameCanvas'
 							style={{width: "75%", height: "45%", border:'1px solid #000000' }}></canvas>
 					</div>
+					<div>
+						<Button onClick={this.draw}>Start</Button>
+					</div>
 				</div>
 			</Layout>
 		)
@@ -29,3 +52,15 @@ class Breakout extends React.Component {
 }
 
 export default Breakout;
+
+const Button = styled.button`
+	font-family: 'Roboto Mono', monospace;
+	text-decoration: ${props => props.disabled ? 'line-through' : 'none' }
+	color: black;
+	background: white;
+	font-size: 1em;
+	margin: 0.5em 1em;
+	padding: 0.25em 1em;
+  border: 2px solid black;
+	border-radius: 2px;
+`
