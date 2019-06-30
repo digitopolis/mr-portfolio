48e790613353b248b00a045b94cfd7ae7d458817
Matt Readout <matt.readout@gmail.com>
2019-06-30 11:34:55 -0400

    add canvas and define context

--- src/pages/breakout.js
+++ src/pages/breakout.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-import React from 'react'
-import PropTypes from 'prop-types'
+let canvas
+let ctx
 
 class Breakout extends React.Component {
+
+	componentDidMount() {
+		canvas = document.getElementById('gameCanvas')
+		ctx = canvas.getContext('2d')
+	}
+
 	render () {
 		return (
 			<Layout>
 				<div className='content'>
 					<h1>Break some blocks</h1>
+					<p>Please be gentle, Atari lawyers</p>
+					<div>
+						<canvas
+							id='gameCanvas'
+							style={{width: "75%", height: "45%", border:'1px solid #000000' }}></canvas>
+					</div>
 				</div>
 			</Layout>
 		)
