e5dd1503beb370e411fa8d995ecc0d984267a666
Matt Readout <matt.readout@gmail.com>
2019-09-15 18:13:42 -0400

    create page/component for breathing exercise

---
+++ src/pages/breathe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+class Breathe extends React.Component {
+	render () {
+		return (
+			<h1>Breathing exercise</h1>
+		)
+	}
+}
+
+export default Breathe;
